7981f73263b3fc3241c3857a225c1ab280d66520
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 11:51:06 +0700

    Update database.php

--- database.php
+++ database.php
@@ -3,3 +3,9 @@ $host = 'localhost';
 $dbname = 'crud_buku' ;
 $username = 'root';
 $password = '';
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=crud_buku;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
