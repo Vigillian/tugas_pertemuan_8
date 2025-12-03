0e85ba4afbf0a845403bffad0eff83535fb8cb02
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 12:15:59 +0700

    Create buku table with relevant fields

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
+CREATE TABLE buku (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    judul VARCHAR(150) NOT NULL,
+    penulis VARCHAR(100) NOT NULL,
+    tahun_terbit INT NOT NULL,
+    kategori VARCHAR(50) NOT NULL,
+    cover VARCHAR(255) NOT NULL,
+    status ENUM('tersedia','habis') NOT NULL DEFAULT 'tersedia'
+);
 
