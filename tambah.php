245666e4881d653a159d3fefa23140c8cefff2d3
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 11:58:16 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -5,3 +5,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $tahun = $_POST['tahun_terbit'];
     $kategori = $_POST['kategori'];
     $cover = $_FILES['cover'];
+ if ($cover['error'] === 0) {
+        $allowedTypes = ['image/jpeg', 'image/png'];
+        $maxSize = 2 * 1024 * 1024;
+        if (!in_array($cover['type'], $allowedTypes)) {
+            echo "Tipe file tidak didukung.";
+            exit;
+        }
+        if ($cover['size'] > $maxSize) {
+            echo "Ukuran file terlalu besar.";
+            exit;
+        }
+        $fileName = time() . "_" . $cover['name'];
+        $uploadPath = "../uploads/" . $fileName;
+        move_uploaded_file($cover['tmp_name'], $uploadPath);
+        $stmt = $pdo->prepare("
+            INSERT INTO buku (judul, penulis, tahun_terbit, kategori, cover, status)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ");
