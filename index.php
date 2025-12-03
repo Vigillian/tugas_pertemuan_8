b4ccbbb4e3a60d55418f9d591a557ac8d3a73eca
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 11:47:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Barang</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-    <body>
-        <header>
-            <h1>Pengelolaan Data Barang</h1>
-        </header>
-
-        <nav>
-             <a href="tambah_barang.php">Tambah Barang</a>
-        </nav>
-    </body>
+$repo = new BookRepository($pdo);
+$buku = $repo->getAll();
+?>
