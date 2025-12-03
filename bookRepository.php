d98b5366a74c0538e15999953860d43d0a9132d2
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 11:53:50 +0700

    Update bookRepository.php

--- bookRepository.php
+++ bookRepository.php
@@ -11,3 +11,10 @@ public function getAll(): array
         $stmt = $this->db->query("SELECT * FROM buku ORDER BY id DESC");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+public function getById(int $id): ?array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM buku WHERE id = ?");
+        $stmt->execute([$id]);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $data ?: null;
+    }
