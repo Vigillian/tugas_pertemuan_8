eaa6362ca666dfe9ec35418988ebfaeeb1d70d50
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 12:01:47 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,1 +1,11 @@
-
+<?php
+require_once __DIR__ . '/../src/BookRepository.php';
+$repo = new BookRepository($pdo);
+if (!isset($_GET['id'])) {
+    die("ID tidak ditemukan.");
+}
+$id = (int) $_GET['id'];
+$data = $repo->getById($id);
+if (!$data) {
+    die("Data buku tidak ditemukan.");
+}
