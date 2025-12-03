62d840ba233a4a464d14f211b5aff4c0b2c089e2
Vigillian <Katsuhikoryuhito@gmail.com>
2025-12-03 11:57:36 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -1,1 +1,7 @@
-
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $judul = $_POST['judul'];
+    $penulis = $_POST['penulis'];
+    $tahun = $_POST['tahun_terbit'];
+    $kategori = $_POST['kategori'];
+    $cover = $_FILES['cover'];
